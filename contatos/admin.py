2497df771073447ab7cf784011e6bfb628483cb1
GusGBalbino <gustavo.gbalbino@hotmail.com>
2023-03-07 10:22:32 -0300

    Feat: Condicionais

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,11 +3,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'telefone', 'email', 'categoria') #Informações disponíveis
+    list_display = ('nome', 'sobrenome', 'telefone', 'email', 'categoria','mostrar') #Informações disponíveis
     list_display_links = ('nome', 'sobrenome') #Link para edição do contato nas categorias 
     list_filter = ('nome', 'sobrenome') #Filtros por categoria
     list_per_page = 10 #Contatos por página
     search_fields = ('nome', 'sobrenome', 'telefone') #Campo de pesquisa
+    list_editable = ('telefone', 'mostrar')
 
 
 admin.site.register(Categoria)
