cd3886dd3c7671ed75bab85384d12b93fa1417db
GusGBalbino <gustavo.gbalbino@hotmail.com>
2023-03-07 11:04:14 -0300

    Feat: Paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.http import Http404
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos,1)
+
+    pageNumber = request.GET.get('page')
+    contatos  = paginator.get_page(pageNumber)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
